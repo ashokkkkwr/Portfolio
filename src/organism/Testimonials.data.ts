02956575cade43a6d56eebaf22ea83046d4439c0
Your Name <you@example.com>
2024-09-21 19:53:47 +0545

    new feature added

--- src/organism/Testimonials.data.ts
+++ src/organism/Testimonials.data.ts
@@ -1,31 +1,31 @@
 export const items = [
   {
       status: "inactive",
-      imgSrc: "https://placedog.net/250/250",
-      name: "Spot",
-      position: "Hide and Seek Master",
+      imgSrc: "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?cs=srgb&dl=pexels-simon-robben-55958-614810.jpg&fm=jpg",
+      name: "Bijaya Shah",
+      position: "Full Stack Developer",
       quote: "Woof. Woof woof woof woof woof. Woof woof! Woof. Woof. Woof woof woof woof woof. Woof woof! Woof. Woof. Woof woof woof woof woof. Woof woof! Woof."
   },
   {
       status: "inactive",
-      imgSrc: "https://placedog.net/350/350",
+      imgSrc: "https://static.vecteezy.com/system/resources/thumbnails/026/829/465/small/beautiful-girl-with-autumn-leaves-photo.jpg",
       name: "Hammy",
       position: "Lunchtime Leader", 
-      quote: "Woof woof woof woof woof woof woof. Woof woof woof. Woof woof woof woof! Woof. Woof woof woof woof woof woof woof woof. Woof woof. Woof woof woof. Woof woof woof woof woof woof woof woof. Woof Woof woof. Woof woof woof. Woof!"
+      quote: "Woof woof woof woof woof woof woof. Woof woof woof. Woof woof woof woof! Woof. Woof woof woof woof woof woof woof woof. Woof woof. Woof woof woof."
   },
   {
       status: "inactive",
-      imgSrc: "https://placedog.net/300/300",
+      imgSrc: "https://www.shutterstock.com/shutterstock/photos/2122700972/display_1500/stock-photo-image-of-young-asian-woman-company-worker-in-glasses-smiling-and-holding-digital-tablet-standing-2122700972.jpg",
       name: "Mario",
       position: "Professional Hero",
       quote: "Woof woof woof woof woof woof woof woof. Woof Woof woof. Woof woof woof. Woof woof woof woof woof woof woof woof. Woof Woof woof. Woof woof woof. Woof!"
   },
   {
       status: "active",
-      imgSrc: "https://placedog.net/450/450",
+      imgSrc: "https://img.freepik.com/free-photo/handsome-confident-smiling-man-with-hands-crossed-chest_176420-18743.jpg",
       name: "Sniffer",
       position: "Guide Dog",
-      quote: "Woof. Woof woof woof woof woof. Woof woof! Woof. Woof. Woof woof woof woof woof. Woof woof! Woof. Woof. Woof woof woof woof woof. Woof woof! Woof. Woof woof woof woof woof woof woof."
+      quote: "Woof. Woof woof woof woof woof. Woof woof! Woof. Woof. Woof woof woof woof woof. Woof woof! Woof. Woof. Woof woof woof woof woof. Woof woof! Woof."
   },
   {
       status: "inactive",
--- src/organism/Testimonials.tsx
+++ src/organism/Testimonials.tsx
@@ -1,8 +1,6 @@
-// import { ReactComponent as ArrowLeft } from '../assets/chevron-left-solid.svg';
-// import { ReactComponent as ArrowRight } from '../assets/chevron-right-solid.svg';
-// import { ReactComponent as Circle } from '../assets/circle-solid.svg';
 import React, { useEffect, useState } from "react";
 import { items } from "../organism/Testimonials.data";
+import { FaAngleLeft, FaAngleRight, FaChevronLeft } from "react-icons/fa";
 
 const Carousel: React.FC = () => {
   const [leftCard, setLeftCard] = useState<number>(0);
@@ -22,6 +20,7 @@ const Carousel: React.FC = () => {
       setRightCard(activeItemIndex + 1 >= length ? 0 : activeItemIndex + 1);
     }
   }, []);
+
   const activatePreviousSlide = () => {
     const newLeftCard = leftCard - 1 < 0 ? arrayLength - 1 : leftCard - 1;
     setRightCard(activeIndex);
@@ -35,7 +34,6 @@ const Carousel: React.FC = () => {
     setActiveIndex(rightCard);
     setRightCard(newRightCard);
   };
-
   const trackerArray = Array.from({ length: arrayLength }, (_, index) => (
     <div className="transform scale-75" key={index}></div>
   ));
@@ -44,58 +42,61 @@ const Carousel: React.FC = () => {
     <div className="flex flex-col items-center bg-antiquewhite text-center bg-gray-200">
       <div className="flex w-full">
         <div
-          className={`card ${
+          className={`card prev mx-2 my-20 bg-ivory p-8 w-1/3 h-48 shadow-lg bg-white rounded-full ${
             leftCard === -1 ? "hidden" : ""
-          } mx-2 my-20 bg-ivory p-8 w-1/3 h-48 shadow-lg bg-white`}
+          }`}
         >
           <img
-            className="rounded-full h-24 -mt-24 ml-[225px] "
+            className="rounded-full h-24 -mt-24 ml-[175px]"
             src={items[leftCard].imgSrc}
             alt="avatar"
           />
           <div className="overflow-hidden">
-            <h3 className="mt-2 mb-0.5">{items[leftCard].name}</h3>
-            <i className="block">{items[leftCard].position}</i>
+            <h3 className="mt-2 mb-0.5 font-bold font-poppins text-xl">{items[leftCard].name}</h3>
+            <i className="block mb-5">{items[leftCard].position}</i>
             <p className="px-3">{items[leftCard].quote}</p>
           </div>
         </div>
         <div
-          className={`card active mx-2 my-20 bg-ivory p-8 w-1/3 h-48 shadow-lg bg-white`}
+          className={`card active mx-2 my-20 bg-ivory p-8 w-[100vh] h-48 shadow-lg bg-white rounded-full`}
         >
           <img
-            className="rounded-full h-24 -mt-24 ml-[225px]"
+            className="rounded-full h-24 -mt-24 ml-[310px]"
             src={items[activeIndex].imgSrc}
             alt="avatar"
           />
           <span>
             <div className="overflow-hidden">
-              <h3 className="mt-2 mb-0.5">{items[activeIndex].name}</h3>
-              <i className="block">{items[activeIndex].position}</i>
+              <h3 className="mt-2 mb-0.5 font-bold font-poppins text-xl">{items[activeIndex].name}</h3>
+              <i className="block mb-5">{items[activeIndex].position}</i>
               <p className="px-3">{items[activeIndex].quote}</p>
             </div>
             <div className="flex justify-between -mt-20 relative">
               <button onClick={activatePreviousSlide}>
-                ArrowLeft SVG
+              <FaAngleLeft className="text-3xl"/>
+
+
               </button>
               <button onClick={activateNextSlide}>
-                ArrowRight SVG
+              <FaAngleRight className="text-3xl"/>
+
               </button>
             </div>
           </span>
         </div>
         <div
-          className={`card ${
+          className={`card next mx-2 my-20 bg-ivory p-8 w-1/3 h-48 shadow-lg bg-white rounded-full ${
             rightCard === -1 ? "hidden" : ""
-          } mx-2 my-20 bg-ivory p-8 w-1/3 h-48 shadow-lg bg-white`}
+          }`}
         >
           <img
-            className="rounded-full h-24 -mt-24 ml-[225px]"
+            className="rounded-full h-24 -mt-24 ml-[187px]"
             src={items[rightCard].imgSrc}
             alt="avatar"
           />
           <div className="overflow-hidden">
-            <h3 className="mt-2 mb-0.5">{items[rightCard].name}</h3>
-            <i className="block">{items[rightCard].position}</i>
+            <h3 className="mt-2 mb-0.5 font-bold font-poppins text-xl">{items[rightCard].name}</h3>
+            <i className="block mb-5">{items[rightCard].position}</i>
             <p className="px-3">{items[rightCard].quote}</p>
           </div>
         </div>
@@ -103,7 +104,6 @@ const Carousel: React.FC = () => {
       <div className="flex justify-center h-10 mt-4">{trackerArray}</div>
     </div>
   );
-}
-
+};
 
 export default Carousel;
